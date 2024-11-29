d9d5df3421db3a131188fbffc4f96a45b1fb39d9
Muhammad Imran <mimran114@gmail.com>
2024-11-29 07:23:43 +0100

    feature-2

--- feature.js
+++ feature.js
@@ -1,4 +1,4 @@
 console.log("first commit  for feature");
 console.log("second commit  for feature");
 console.log("this is master branch");
-console.log("this is conflict branch");
+console.log("this is feature-2");
