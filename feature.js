c9fdc384a52faeaba8659ae153c329852c5bf8f7
Muhammad Imran <mimran114@gmail.com>
2024-11-29 07:18:19 +0100

    feature 1 changes

--- feature.js
+++ feature.js
@@ -1,4 +1,4 @@
 console.log("first commit  for feature");
 console.log("second commit  for feature");
 console.log("this is master branch");
-console.log("this is conflict branch");
+console.log("trying to create conflict");
